007a43324417bcc5df805553552987c8e6d6b620
muntasir2165 <muntasir2165@hotmail.com>
2014-06-08 03:44:31 -0600

    Python game projects using simplegui in CodeSkulptor

--- 1_rock_paper_scissors_lizard_Spock.py
+++ 1_rock_paper_scissors_lizard_Spock.py
@@ -1,94 +1,81 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1265\cocoasubrtf190
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural
+# Rock-paper-scissors-lizard-Spock template
 
-\f0\fs24 \cf0 # Rock-paper-scissors-lizard-Spock template\
-\
-import random\
-# The key idea of this program is to equate the strings\
-# "rock", "paper", "scissors", "lizard", "Spock" to numbers\
-# as follows:\
-#\
-# 0 - rock\
-# 1 - Spock\
-# 2 - paper\
-# 3 - lizard\
-# 4 - scissors\
-\
-# helper functions\
-\
-def name_to_number(name):\
-    # delete the follwing pass statement and fill in your code below\
-    # convert name to number using if/elif/else\
-    # don't forget to return the result!\
-    if name == 'rock':\
-        return 0\
-    elif name == 'Spock':\
-        return 1\
-    elif name == 'paper':\
-        return 2\
-    elif name == 'lizard':\
-        return 3\
-    elif name == 'scissors':\
-        return 4\
-    else:\
-        print 'Unrecognized choice. Please choose one of: rock, Spock, paper, lizard, scissors'\
-\
-\
-def number_to_name(number):\
-    # delete the follwing pass statement and fill in your code below\
-    # convert number to a name using if/elif/else\
-    # don't forget to return the result!\
-    if number == 0:\
-        return 'rock'\
-    elif number == 1:\
-        return 'Spock'\
-    elif number == 2:\
-        return 'paper'\
-    elif number == 3:\
-        return 'lizard'\
-    elif number == 4:\
-        return 'scissors'\
-    else:\
-        print 'Unrecognized choice. Please choose a number between 0 and 4, inclusive'\
-\
-    \
-\
-def rpsls(player_choice): \
-    # delete the follwing pass statement and fill in your code below\
-    \
-    # print a blank line to separate consecutive games\
-    print\
-    # print out the message for the player's choice\
-    print 'Player chooses ' + player_choice\
-    # convert the player's choice to player_number using the function name_to_number()\
-    player_number = name_to_number(player_choice)\
-    # compute random guess for comp_number using random.randrange()\
-    comp_number = random.randrange(0, 5)\
-    # convert comp_number to comp_choice using the function number_to_name()\
-    comp_choice = number_to_name(comp_number)\
-    # print out the message for computer's choice\
-    print 'Computer chooses ' + comp_choice\
-    # compute difference of comp_number and player_number modulo five\
-    difference = (comp_number-player_number) % 5\
-    # use if/elif/else to determine winner, print winner message\
-    if difference == 1 or difference == 2:\
-        print 'Computer wins!'\
-    elif difference == 3 or difference == 4:\
-        print 'Player wins!'\
-    else:\
-        print "Player and computer tie!"\
-    \
-# test your code - LEAVE THESE CALLS IN YOUR SUBMITTED CODE\
-rpsls("rock")\
-rpsls("Spock")\
-rpsls("paper")\
-rpsls("lizard")\
-rpsls("scissors")\
-\
-# always remember to check your completed program against the grading rubric\
-\
-\
-}
+import random
+# The key idea of this program is to equate the strings
+# "rock", "paper", "scissors", "lizard", "Spock" to numbers
+# as follows:
+#
+# 0 - rock
+# 1 - Spock
+# 2 - paper
+# 3 - lizard
+# 4 - scissors
+
+# helper functions
+
+def name_to_number(name):
+    # delete the follwing pass statement and fill in your code below
+    # convert name to number using if/elif/else
+    # don't forget to return the result!
+    if name == 'rock':
+        return 0
+    elif name == 'Spock':
+        return 1
+    elif name == 'paper':
+        return 2
+    elif name == 'lizard':
+        return 3
+    elif name == 'scissors':
+        return 4
+    else:
+        print 'Unrecognized choice. Please choose one of: rock, Spock, paper, lizard, scissors'
+
+def number_to_name(number):
+    # delete the follwing pass statement and fill in your code below
+    # convert number to a name using if/elif/else
+    # don't forget to return the result!
+    if number == 0:
+        return 'rock'
+    elif number == 1:
+        return 'Spock'
+    elif number == 2:
+        return 'paper'
+    elif number == 3:
+        return 'lizard'
+    elif number == 4:
+        return 'scissors'
+else:
+        print 'Unrecognized choice. Please choose a number between 0 and 4, inclusive'
+
+def rpsls(player_choice): 
+    # delete the follwing pass statement and fill in your code below
+    
+    # print a blank line to separate consecutive games
+    print
+    # print out the message for the player's choice
+    print 'Player chooses ' + player_choice
+    # convert the player's choice to player_number using the function name_to_number()
+    player_number = name_to_number(player_choice)
+    # compute random guess for comp_number using random.randrange()
+    comp_number = random.randrange(0, 5)
+    # convert comp_number to comp_choice using the function number_to_name()
+    comp_choice = number_to_name(comp_number)
+    # print out the message for computer's choice
+    print 'Computer chooses ' + comp_choice
+    # compute difference of comp_number and player_number modulo five
+    difference = (comp_number-player_number) % 5
+    # use if/elif/else to determine winner, print winner message
+    if difference == 1 or difference == 2:
+        print 'Computer wins!'
+    elif difference == 3 or difference == 4:
+        print 'Player wins!'
+    else:
+        print "Player and computer tie!"
+    
+# test your code - LEAVE THESE CALLS IN YOUR SUBMITTED CODE
+rpsls("rock")
+rpsls("Spock")
+rpsls("paper")
+rpsls("lizard")
+rpsls("scissors")
+# always remember to check your completed program against the grading rubric
